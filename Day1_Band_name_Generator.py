60ce391a78724847ab232e7ab70a714c716cd410
bharani <orsuleelabharani18@gmail.com>
2026-09-12 21:40:36 +0530

    first commit

---
+++ Day1_Band_name_Generator.py
@@ -0,0 +1,6 @@
+#Band Name Generator
+print("-----hello ! welcome to the Band Name Generator-----")
+city_name = input("Where do you live :")
+pet_name = input("Whats your pet name :")
+
+print(f'Your band name is {city_name} {pet_name}')
